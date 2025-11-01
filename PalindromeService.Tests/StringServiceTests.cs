0afca4006b57ac8bd986c81265082e67d918811a
Gerald Muteru <muterugerald@gmail.com>
2025-11-01 17:07:14 +0300

    Initial commit

---
+++ PalindromeService.Tests/StringServiceTests.cs
@@ -0,0 +1,28 @@
+﻿// File: PalindromeService.Tests/StringServiceTests.cs
+using Xunit;
+using PalindromeService; // <-- This is your app's code!
+
+namespace PalindromeService.Tests
+{
+    public class StringServiceTests
+    {
+        [Fact]
+        public void IsPalindrome_ShouldReturnTrueForPalindrome()
+        {
+            // Arrange
+            // TODO: Create an instance of your StringService
+            StringService stringserv = new StringService();
+            // TODO: Create a test string (e.g., "A man, a plan, a canal: Panama")
+            string a = "A man, a plan, a canal: Panama";
+
+
+            // Act
+            
+            // TODO: Call the IsPalindrome method
+
+            // Assert
+            // TODO: Assert that the result is 'true'
+            Assert.True(false); // Replace this!
+        }
+    }
+}
---
+++ PalindromeService/StringService.cs
@@ -0,0 +1,20 @@
+﻿// File: PalindromeService/StringService.cs
+using System;
+
+namespace PalindromeService
+{
+    public class StringService
+    {
+        // TODO: Implement this method
+        public bool IsPalindrome(string input)
+        {
+            // HINT: You'll need to clean the string (lowercase, remove non-letters)
+            string CleanedString = new string(input.ToLower().Where(char.IsLetter).ToArray());
+            // and then compare the string to its reverse.
+            // (You can use LINQ and new string(...) to reverse it)
+            string ReversedString = new string(CleanedString.Reverse().ToArray());
+            return CleanedString == ReversedString
+            throw new NotImplementedException(); 
+        }
+    }
+}
